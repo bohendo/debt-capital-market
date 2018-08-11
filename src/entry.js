14d25335c55058b11c101ac3031096a71588e58b
Bo Henderson <bohende@gmail.com>
2018-08-11 11:07:06 +0530

    Initial Commit

---
+++ src/entry.js
@@ -0,0 +1,4 @@
+//import Dharma from "@dharmaprotocol/dharma.js";
+//const Dharma = require("@dharmaprotocol/dharma.js");
+const Dharma = require("@dharmaprotocol/dharma.js");
+console.log(typeof Dharma)
